cf57a7c488b999fd8cc2625fc58f726b9b52716a
Baptiste Langlade <baptouuuu@gmail.com>
2018-07-28 10:47:43 +0200

    wrap handler to empty trash to delete relationships first

---
+++ src/Component/Files/Handler/EmptyTrashHandler.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types = 1);
+
+namespace PersonalGalaxy\X\Component\Files\Handler;
+
+use PersonalGalaxy\Files\{
+    Handler\EmptyTrashHandler as Handler,
+};
+use Innmind\Neo4j\DBAL\{
+    Connection,
+    Query\Query,
+    Clause\Expression\Relationship,
+};
+
+final class EmptyTrashHandler
+{
+    private $handle;
+    private $dbal;
+
+    public function __construct(
+        Handler $handle,
+        Connection $dbal
+    ) {
+        $this->handle = $handle;
+        $this->dbal = $dbal;
+    }
+
+    public function __invoke(): void
+    {
+        $this->dbal->execute(
+            (new Query)
+                ->match(null, ['Files'])
+                ->withProperty('trashed', '{trashed}')
+                ->withParameter('trashed', true)
+                ->linkedTo(null, ['Files'])
+                ->through('CHILD_OF', 'rels', Relationship::LEFT)
+                ->withAnyDistance()
+                ->foreach('(rel in rels | DELETE rel)')
+        );
+        ($this->handle)();
+    }
+}
---
+++ tests/Component/Files/Handler/EmptyTrashHandlerTest.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\PersonalGalaxy\X\Component\Files\Handler;
+
+use PersonalGalaxy\X\Component\Files\Handler\EmptyTrashHandler;
+use PersonalGalaxy\Files\{
+    Handler\EmptyTrashHandler as Handler,
+    Handler\RemoveFileHandler,
+    Handler\RemoveFolderHandler,
+    Repository\FileRepository,
+    Repository\FolderRepository,
+};
+use Innmind\Filesystem\Adapter;
+use Innmind\Neo4j\DBAL\Connection;
+use Innmind\Immutable\Set;
+use PHPUnit\Framework\TestCase;
+
+class EmptyTrashHandlerTest extends TestCase
+{
+    public function testInvokation()
+    {
+        $handle = new EmptyTrashHandler(
+            new Handler(
+                $files = $this->createMock(FileRepository::class),
+                $folders = $this->createMock(FolderRepository::class),
+                $removeFile = new RemoveFileHandler(
+                    $files,
+                    $this->createMock(Adapter::class)
+                ),
+                new RemoveFolderHandler(
+                    $files,
+                    $folders,
+                    $removeFile
+                )
+            ),
+            $dbal = $this->createMock(Connection::class)
+        );
+        $dbal
+            ->expects($this->once())
+            ->method('execute')
+            ->with($this->callback(static function($query): bool {
+                return (string) $query === 'MATCH (:Files { trashed: {trashed} })<-[rels:CHILD_OF*]-(:Files) FOREACH (rel in rels | DELETE rel)' &&
+                    $query->parameters()->get('trashed')->value() === true;
+            }));
+
+        $this->assertNull($handle());
+    }
+}
