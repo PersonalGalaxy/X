fda14c90690f5ee26ee15ca88135c2d7bf4d54cf
Baptiste Langlade <baptouuuu@gmail.com>
2018-07-28 10:35:46 +0200

    wrap handler to remove a folder to delete relationships first

---
+++ src/Component/Files/Handler/RemoveFolderHandler.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types = 1);
+
+namespace PersonalGalaxy\X\Component\Files\Handler;
+
+use PersonalGalaxy\Files\{
+    Command\RemoveFolder,
+    Handler\RemoveFolderHandler as Handler,
+};
+use Innmind\Neo4j\DBAL\{
+    Connection,
+    Query\Query,
+    Clause\Expression\Relationship,
+};
+
+final class RemoveFolderHandler
+{
+    private $handle;
+    private $dbal;
+
+    public function __construct(
+        Handler $handle,
+        Connection $dbal
+    ) {
+        $this->handle = $handle;
+        $this->dbal = $dbal;
+    }
+
+    public function __invoke(RemoveFolder $wished): void
+    {
+        $this->dbal->execute(
+            (new Query)
+                ->match(null, ['Files', 'Folder'])
+                ->withProperty('identity', '{folder}')
+                ->withParameter('folder', (string) $wished->identity())
+                ->linkedTo(null, ['Files'])
+                ->through('CHILD_OF', 'rels', Relationship::LEFT)
+                ->withAnyDistance()
+                ->foreach('(rel in rels | DELETE rel)')
+        );
+        ($this->handle)($wished);
+    }
+}
