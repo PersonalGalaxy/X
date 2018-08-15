3039122076814c8c227950a322e6492ccc4e5447
Baptiste Langlade <baptouuuu@gmail.com>
2018-08-15 15:53:42 +0200

    add 2fa code verification

--- src/Web/Authentication/FormResolver.php
+++ src/Web/Authentication/FormResolver.php
@@ -3,12 +3,17 @@ declare(strict_types = 1);
 
 namespace PersonalGalaxy\X\Web\Authentication;
 
-use PersonalGalaxy\X\Web\Exception\UserNotFound;
+use PersonalGalaxy\X\Web\Exception\{
+    UserNotFound,
+    TwoFactorCodeRequired,
+};
 use PersonalGalaxy\Identity\{
     Entity\Identity\Email,
     Command\Identity\VerifyPassword,
+    Command\Identity\Validate2FACode,
     Specification\Identity\Email as Spec,
     Repository\IdentityRepository,
+    TwoFactorAuthentication\Code
 };
 use Innmind\HttpAuthentication\{
     ViaForm\Resolver,
@@ -46,6 +51,17 @@ final class FormResolver implements Resolver
             $identity->identity(),
             $form->get('password')->value()
         ));
+
+        if ($identity->twoFactorAuthenticationEnabled()) {
+            if (!$form->has('2fa_code')) {
+                throw new TwoFactorCodeRequired;
+            }
+
+            $this->commandBus->handle(new Validate2FACode(
+                $identity->identity(),
+                new Code($form->get('2fa_code')->value())
+            ));
+        }
 
         return $identity->identity();
     }
---
+++ src/Web/Exception/TwoFactorCodeRequired.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types = 1);
+
+namespace PersonalGalaxy\X\Web\Exception;
+
+final class TwoFactorCodeRequired extends RuntimeException
+{
+}
